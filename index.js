76f20260e57319682a42df2a358865cee64745bb
Olivia Lynn <olynn@andrew.cmu.edu>
2023-08-09 11:18:08 -0700

    try new package npm-walker

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const walk = require("walk");
+//const walk = require("walk");
+const walker = require("node-walker");
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -10,13 +11,12 @@ try {
   const time = new Date().toTimeString();
   core.setOutput("time", time);
   */
-
   // Get the JSON webhook payload for the event that triggered the workflow
   /*
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
   */
-
+  /*
   const walker = walk.walk(".", {
     followLinks: false,
     filters: ["node_modules"],
@@ -29,6 +29,30 @@ try {
     }
     next();
   });
+  */
+  walker(
+    ".",
+
+    function (errorObject, fileName, fnNext) {
+      // an error occurred
+      if (errorObject) throw errorObject;
+
+      // a filename has been provided
+      if (fileName !== null) {
+        // do something with that filename
+        console.log("File: " + fileName);
+      }
+
+      // all files have been read, fileName is null
+      if (fileName === null) {
+        // continue with some other task
+        return;
+      }
+
+      // call next(); when you want to proceed
+      if (fnNext) fnNext();
+    }
+  );
 } catch (error) {
   core.setFailed(error.message);
 }
